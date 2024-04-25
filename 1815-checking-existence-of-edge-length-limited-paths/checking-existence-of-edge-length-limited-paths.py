bacd961279c297ce3872ffd9de7b2d05e4156e0e
neba378 <137883428+neba378@users.noreply.github.com>
2024-04-25 19:34:28 +0300

    Time: 1492 ms (31.67%) | Memory: 63.5 MB (21.67%) - LeetSync

---
+++ 1815-checking-existence-of-edge-length-limited-paths/checking-existence-of-edge-length-limited-paths.py
@@ -0,0 +1,71 @@
+class UnionFind:
+    def __init__(self, size):
+        self.parent = {i:i for i in range(size)} # Using dictionary
+        # self.parent = [i for i in range(size)] # Using list
+        self.rank = defaultdict(int)
+        
+    def find(self, x):
+        
+        while(x != self.parent[x]):
+            x = self.parent[x]
+        return x
+		
+    def union(self, x, y):
+        px = self.find(x)
+        py = self.find(y)
+        if py!=px:
+            if self.rank[py]>self.rank[px]:
+                self.parent[px] = py
+            elif self.rank[py]<self.rank[px]:
+                self.parent[py] = px
+            else:
+                self.parent[py] = px
+                self.rank[px]+=1
+                
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
+
+
+class Solution:
+    def distanceLimitedPathsExist(self, n: int, ed: List[List[int]], q: List[List[int]]) -> List[bool]:
+        uni = UnionFind(n)
+        ans = ["" for i in range(len(q))]
+        for i in range(len(q)):
+            q[i].append(i)
+        q.sort(key = lambda x: x[2])
+        ed.sort(key = lambda x:x[2])
+        j = 0
+        for i in q:
+            while j<len(ed) and ed[j][2]<i[2]:
+                uni.union(ed[j][0],ed[j][1])
+                j+=1
+            if uni.find(i[0]) == uni.find(i[1]):
+                ans[i[3]] = True
+            else:
+                ans[i[3]] = False
+        return ans
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
