07599135a1fd3743763ece5a1b62174467b235ce
neba378 <137883428+neba378@users.noreply.github.com>
2024-05-09 19:16:32 +0300

    Time: 32 ms (79.71%) | Memory: 16.5 MB (89.68%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,7 +1,9 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums)<=2:
-            return max(nums)
+        if len(nums)==2:
+            return nums[0] if nums[0]>nums[1] else nums[1]
+        elif len(nums) == 1:
+            return nums[0]
         nums[-3]+=nums[-1]
         for i in range(len(nums)-4,-1,-1):
             nums[i]+=nums[i+2] if nums[i+2]>nums[i+3] else nums[i+3]
