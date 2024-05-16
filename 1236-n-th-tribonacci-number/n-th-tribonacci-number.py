4fbce4ce0f19a6accb9505fccc337f5cc9a185de
neba378 <137883428+neba378@users.noreply.github.com>
2024-05-16 18:25:12 +0300

    Time: 31 ms (77.80%) | Memory: 16.6 MB (32.58%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,12 +1,17 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        dic = {1:1,0:0,2:1}
-        def rec(n):
-            if n<=1:
-                return n
-            if n == 2:
-                return 1
-            if n not in dic:
-                dic[n] = rec(n-3)+rec(n-2)+rec(n-1)
-            return dic[n]
-        return rec(n)
+        # dic = {1:1,0:0,2:1}
+        # def rec(n):
+        #     if n<=1:
+        #         return n
+        #     if n == 2:
+        #         return 1
+        #     if n not in dic:
+        #         dic[n] = rec(n-3)+rec(n-2)+rec(n-1)
+        #     return dic[n]
+        # return rec(n)
+
+        dp = [0,1,1]
+        for i in range(n-2):
+            dp.append(dp[-1]+dp[-2]+dp[-3])
+        return dp[n]
