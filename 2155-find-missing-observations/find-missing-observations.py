8558df4a610298ee0219d4272e0c7fff18cfed9b
Nebiyu Musbah <nebiyumusbah378@gmail.com>
2024-09-05 17:03:44 +0300

    Time: 1010 ms (38.23%) | Memory: 27.1 MB (45.99%) - LeetSync

---
+++ 2155-find-missing-observations/find-missing-observations.py
@@ -0,0 +1,25 @@
+class Solution:
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        l = len(rolls)+n
+        s = sum(rolls)
+        x = l*mean-s
+        if x>n*6 or x<(n):
+            return []
+        else:
+            quo = x//n
+            final = quo*n
+            less = x-final
+            if less == 0:
+                return [quo]*n
+            else:
+                lst = [quo]*(n-1)
+                if less+quo>6:
+                    print("eyes")
+                    for i in range(less+quo-6):
+                        lst[i]+=1
+                    if 7 in lst:
+                        return []
+                    return lst + [6]
+
+                return lst + [less+quo] 
+        return []
