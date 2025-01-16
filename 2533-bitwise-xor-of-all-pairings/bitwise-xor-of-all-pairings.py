6d8e3f905e463d9c1608edd4be66413997e05aaa
Nebiyu Musbah <nebiyumusbah378@gmail.com>
2025-01-16 08:29:17 +0300

    Time: 5 ms (46.24%) | Memory: 36.7 MB (27.17%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.py
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.py
@@ -1,14 +1,12 @@
 class Solution:
     def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
         l1,l2 = len(nums1),len(nums2)
-        dic = defaultdict(int)
-        for num in nums1:
-            dic[num]+=l2
-        for num in nums2:
-            dic[num]+=l1
         x = 0
-        for num in dic:
-            if dic[num]%2:
+        if l2%2:
+            for num in nums1:
+                x^=num
+        if l1%2:
+            for num in nums2:
                 x^=num
         return x
         
